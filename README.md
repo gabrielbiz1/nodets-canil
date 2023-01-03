bb740d6bbe1cb2e397d0a8ccb263e34446774fda
gabriel biz <gabrielbizcontato@gmail.com>
2023-01-03 19:00:54 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # nodets-canil
+
+Projeto do curso Node + Typescript
+
+### Pré-requisitos globais:
+
+`npm install -g nodemon typescript ts-node`
+
+### Instalação
+
+`npm install`
+
+### Para rodar o projeto
+
+`npm run start-dev`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res: Response) => {
+  res.send('Página home')
+};
+export const dogs = (req: Request, res: Response) => {
+  res.send('Página home')
+};
+export const cats = (req: Request, res: Response) => {
+  res.send('Página home')
+};
+export const fishers = (req: Request, res: Response) => {
+  res.send('Página home')
+};
