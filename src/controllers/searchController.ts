8805fc2a4f3b96bb17281548ce03b3ffeda2756c
gabriel biz <gabrielbizcontato@gmail.com>
2023-01-04 13:23:41 -0300

    final project

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const search = (req: Request, res: Response) => {
-  res.send('Página home')
+  let query: string = req.query.q as string;
+  let list = Pet.getFromName(query)
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list,
+    query
+  });
 };
